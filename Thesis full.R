daf35661a13facaa0f927b4d1198e2e5655b5cc3
NicolaPilia <59473508+NicolaPilia@users.noreply.github.com>
2020-07-03 23:38:57 +0200

    03/07/2020

--- Thesis full.R
+++ Thesis full.R
@@ -1,2 +1,59 @@
-library(tm)
-data=c(1,2)
+## Loading and cleaning the data
+#x <- readLines("netflix.csv")
+data<- read.csv("netflix.csv", header =TRUE, sep=";")
+#Viewing complete dataset
+View(data)
+#str(data)
+
+#Separating text variables to other variables
+cons<-data$cons
+pros<-data$pros
+
+View(pros)
+
+#Converting foreign characters into english
+library(gsubfn)
+cons<-gsub("?","c",cons)
+cons<-gsub("?",",",cons)
+cons<-gsub("f","f",cons)
+cons<-gsub("?","y",cons)
+cons<-gsub("??","",cons)
+cons<-gsub("*","",cons)
+
+pros<-gsub("?","c",pros)
+pros<-gsub("?",",",pros)
+pros<-gsub("f","f",pros)
+pros<-gsub("?","y",pros)
+pros<-gsub("??","",pros)
+pros<-gsub("*","",pros)
+
+
+corp_p<-Corpus(VectorSource(pros))
+corp_c<-Corpus(VectorSource(cons))
+
+corp_p<-tm_map(corp_p,stripWhitespace)
+corp_p<-tm_map(corp_p, removePunctuation)
+corp_p<-tm_map(corp_p, removeNumbers)
+#stopwords
+
+corp_p<-tm_map(corp_p, removeWords,stopwords("english"))
+#stemming
+corp_p<-tm_map(corp,stemDocument)
+inspect(tdm_p)
+inspect(corp_p)
+?tdm_p<-TermDocumentMatrix(corp_p)
+#modifing words to differentiate between positive and negative terms.
+tdm_p$dimnames$Terms <- paste0("P_",tdm$dimnames$Terms)
+
+tfidf_p<-weightTfIdf(tdm)
+inspect(tfidf_p)
+
+#TF-IDF and latent semantic analysis
+tdm<-TermDocumentMatrix(corp)
+tfidf<-weightTfIdf(tdm)
+
+library(lsa)
+lsa.tfidf<-lsa(tfidf,dim=20)
+
+words.df<-as.data.frame(as.matrix(lsa.tfidf$dk))
+View(words.df) 
