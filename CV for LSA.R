59464462cbeb9b6e3eab9ae0b59edd96584c3f97
NicolaPilia <59473508+NicolaPilia@users.noreply.github.com>
2020-07-07 19:32:49 +0200

    7/7/20 commit

---
+++ CV for LSA.R
@@ -0,0 +1,43 @@
+##Correct K-Fold with right measure
+library(caret)
+library(MLmetrics)
+#Creating dataset for K-fold and making adjustments for training the model
+K_Fold_Data<-cbind(output=data$over.b,words.df)
+K_Fold_Data$output<-as.factor(K_Fold_Data$output)
+K_Fold_Data$output<-make.names(K_Fold_Data$output)
+
+#Creating the list for the output metrix
+MySummary  <- function(data, lev = NULL, model = NULL){
+  a1 <- defaultSummary(data, lev, model)
+  b1 <- twoClassSummary(data, lev, model)
+  c1 <- prSummary(data, lev, model)
+  out <- c(a1, b1, c1)
+  out}
+#Implementing CV
+ctrl <- trainControl(method = "cv",number = 10,savePredictions = TRUE, summaryFunction = MySummary,classProbs = TRUE)
+
+#traing the model and looking at the results.
+mod_fit <- train(output~.,data =K_Fold_Data,method = "glm",trControl = ctrl)
+mod_fit$results
+View(mod_fit$results)
+
+#using this code to create the df DataFrame to compare different numbers of LSA
+mod_fit <- train(output~.,data =K_Fold_Data,method = "glm",trControl = ctrl)
+df[20,]<- mod_fit$results[1,]
+
+#making adjustments at the df
+df[,1]<-1:20
+colnames(df)[1]<-"number of LSA"
+
+#Creating the plot for the LSA CV
+plot(df$number_of_LSA,df$Accuracy,type='l',xaxt='n',xlab = "Number of LSA", ylab="Accuracy",ylim = c(0.6,0.85), lwd=2, col="blue")
+lines(df$number_of_LSA,df$Spec,col="green", lwd=2,)
+lines(df$number_of_LSA,df$Sens,col="red", lwd=2)
+legend("bottomright",legend=c("Accuracy","Specificity","Sensitivity"), col=c("blue","green","red"),pch = c(20,20,20), bty="n")
+axis(1, xaxp=c(1, 20,19), las=1,xlab = "Number of LSA")
+abline(h=6, v=6, col="gray47", lty=3)
+
+#sensitivity -> 0
+#specificity -> 1
+
+
---
+++ old code for CV.R
@@ -0,0 +1,58 @@
+tfidf_2<-tfidf
+kfold_LSA<-function(y){
+  lsa<-lsa(tfidf_2,dim=y)
+  words.df_5<-as.data.frame(as.matrix(lsa$dk))
+  K_Fold_Data<-cbind(output=data$over.b,words.df)
+  K_Fold_Data$output<-as.factor(K_Fold_Data$output)
+  K_Fold_Data$output<-make.names(K_Fold_Data$output)
+  #traing the model and looking at the results.
+  mod_fit <- train(output~.,data =K_Fold_Data,method = "glm",trControl = ctrl)
+  print(mod_fit$results)
+}
+
+df<-data.frame(matrix(nrow=22, ncol=20))
+
+Kfold_6<-kfold_LSA(6)
+df[6,]<-Kfold_6
+
+
+require(rbind)
+
+K_fold_df<-
+for(i in 1:20){
+  Kfold_i<-kfold_LSA(i)
+  df[i,]<-Kfold_i
+  View(df)
+}
+names(df)<-colnames(mod_fit$results)
+
+
+words.df_5<-as.data.frame(as.matrix(lsa_5$dk))
+
+##Correct K-Fold with right measure
+library(caret)
+library(MLmetrics)
+#Creating dataset for K-fold and making adjustments for training the model
+K_Fold_Data<-cbind(output=data$over.b,words.df)
+K_Fold_Data$output<-as.factor(K_Fold_Data$output)
+K_Fold_Data$output<-make.names(K_Fold_Data$output)
+
+#Creating the list for the output metrix
+MySummary  <- function(data, lev = NULL, model = NULL){
+  a1 <- defaultSummary(data, lev, model)
+  b1 <- twoClassSummary(data, lev, model)
+  c1 <- prSummary(data, lev, model)
+  out <- c(a1, b1, c1)
+  out}
+#Implementing CV
+ctrl <- trainControl(method = "cv",number = 10,savePredictions = TRUE, summaryFunction = MySummary,classProbs = TRUE)
+
+#traing the model and looking at the results.
+mod_fit <- train(output~.,data =K_Fold_Data,method = "glm",trControl = ctrl)
+mod_fit$results
+View(mod_fit$results)
+
+rbind(mod_fit$metric,0)
+ls(mod_fit$results)
+
+colnames(mod_fit$results)
